2498a1c560c1caea58a4a4d635e070fdb6296447
Shikhar Lohiya <shikhar.lohiya@ibgroup.co.in>
2025-05-04 15:28:54 +0530

    branch-list

--- Routes/Master/MasterRoute.js
+++ Routes/Master/MasterRoute.js
@@ -2,11 +2,27 @@ const express = require('express');
 const router = express.Router();
 const MasterController = require('../../Controller/Master/MasterController');
 
+ 
+// const auth = require('../../middleware/check-auth');
+ 
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
+// Upload branch data from Excel file
+router.post('/upload-branch-data', upload.single('file'),  MasterController.uploadBranchData);
+
+
+
+
+
 // Get all zonal managers with their region information
 router.get('/get-all-region', MasterController.getAllRegions);
 router.get('/get-all-zonal-manager', MasterController.getZonalManagers);
 router.post('/zonal-manager', MasterController.addZonalManager);
 router.post('/zonal-managers/status', MasterController.updateZonalManagerStatus);
 router.get('/campaigns', MasterController.getAllCampaigns);
+
+// Get branches for an employee by ID
+router.get('/employee-branches/:employeeId', MasterController.getEmployeeBranchesById);
  
 module.exports = router;
